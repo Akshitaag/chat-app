198b82c5d4df74db54b7d03b9174b40ac6c4d839
Akshita <aggarwalakshita1@gmail.com>
2017-10-20 12:58:40 +0530

    basics of app

--- public/js/index.js
+++ public/js/index.js
@@ -3,16 +3,34 @@ var socket=io();
 	socket.on('connect',function(){
 		console.log("lol");
 
-		socket.emit('createMessage',{
-		from: "aggarwalakshita1@gmail.com",
-		text: " nice to see you"
-	});
+		
 });
 	socket.on('disconnect',function() {
 		console.log("disconnected");
 	});
 	socket.on('newMessage',function(message){
-		console.log("Message is");
+		//console.log("Message is");
 		console.log(message);
+		var li=jQuery('<li></li>');
+		li.text(message.from+":"+message.text);
+		jQuery('#messages').append(li);
+	});
+
+	// socket.emit('createMessage',{
+	// 	from: "aggarwalakshita1@gmail.com",
+	// 	text: " nice to see you"
+	// },function(data){
+	// 	console.log("got it"+data);
+	// });
+
+	jQuery("#message-form").on('submit',function(e){
+		e.preventDefault();
+
+		socket.emit('createMessage',{
+		from: "user",
+		text: jQuery('[name=message]').val()
+	},function(data){
+		console.log("got it"+data);
+	});
 	});
 	
