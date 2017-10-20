e4b11fe73615e8b02d6042aaf24216260da4c0b1
Akshita <aggarwalakshita1@gmail.com>
2017-10-20 14:28:32 +0530

    geolocation api used

--- public/js/index.js
+++ public/js/index.js
@@ -15,7 +15,14 @@ var socket=io();
 		li.text(message.from+":"+message.text);
 		jQuery('#messages').append(li);
 	});
-
+	socket.on('newLocationMessage',function(message){
+		var li=jQuery('<li></li>');
+		var a=jQuery('<a target="_blank">Location</a>');
+		li.text(message.from+":");
+		a.attr('href',message.url);
+		li.append(a);
+		jQuery('#messages').append(li);
+	});
 	// socket.emit('createMessage',{
 	// 	from: "aggarwalakshita1@gmail.com",
 	// 	text: " nice to see you"
@@ -34,3 +41,18 @@ var socket=io();
 	});
 	});
 	
+	var locationButton= jQuery("#send-location");
+	locationButton.on('click',function(){
+		if(!navigator.geolocation){
+			return alert("feature not supported");
+		}
+
+		navigator.geolocation.getCurrentPosition(function(position){
+			socket.emit('createLocationMessage',{
+				latitude: position.coords.latitude,
+				longitude: position.coords.longitude
+			});
+		},function(){
+			alert('Unable fetch to location');
+		});
+	});
